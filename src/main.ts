c5e8c78ef46fedc0c6e5903498c567b88cbb0d36
Junn-work <socker210@gmail.com>
2024-02-15 19:40:07 +0900

    feat(temp): add dnd script

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,15 @@
-const root = document.querySelector('#app')!
+document.addEventListener('DOMContentLoaded', function () {
+  const $dropzone = document.getElementById('dropzone')!
 
-root.innerHTML = '<h1>hahah</h1>'
+  $dropzone.addEventListener('dragover', function (e) {
+    e.preventDefault()
+
+    console.log('called dragover event')
+  })
+
+  $dropzone.addEventListener('drop', function (e) {
+    e.preventDefault()
+
+    console.log('called drop event')
+  })
+})
